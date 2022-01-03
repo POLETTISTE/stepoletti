3e2450a4be2fd47377b36fe902fc79d868fb46bd
STEPHANE POLETTI <polettistephane@msn.com>
2022-01-03 15:37:03 +0100

    change navbar with animation and bootstrap collapse

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,7 +11,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:success] = "MESSAGE ENVOYE!"
+      flash.now[:success] = "MESSAGE ENVOYE! Vous recevrez une réponse rapidement."
     else
       flash.now[:error] = "IMPOSSIBLE D'ENVOYER LE MESSAGE"
       render :new
