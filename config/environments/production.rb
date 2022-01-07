0c63b6f1a653a13b7eebe82c1e13e9971f161c9e
STEPHANE POLETTI <polettistephane@msn.com>
2022-01-07 23:25:32 +0100

    Updating production mailer settings to use MailGun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,7 +124,7 @@ Rails.application.configure do
     address: ENV['MAILGUN_SMTP_SERVER'],
     user_name: ENV['MAILGUN_SMTP_LOGIN'],
     password: ENV['MAILGUN_SMTP_PASSWORD'],
-    domain: 'stephanepoletti.heroku.com', # UPDATE THIS VALUE WITH YOUR OWN APP
+    domain: 'stephanepoletticom.heroku.com', # UPDATE THIS VALUE WITH YOUR OWN APP
     authentication: :plain
   }
   ActionMailer::Base.delivery_method = :smtp
